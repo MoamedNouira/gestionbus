07df17a269306b61c24c04616492769c172b3bde
wajdi077 <methlouthiwajdi@gmail.com>
2017-11-18 22:09:54 +0100

    cc

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var dbConfig = require('./config/database.config.js');
 var mongoose = require('mongoose');
 mongoose.connect(dbConfig.url);
 mongoose.connection.on('error', function() {
-    console.log('Impossible de se connecter à la la base de données. Quitter maintenant ...');
+    console.log('Impossible de se connecter à la la la base de données. Quitter maintenant ...');
     process.exit();
 });
 mongoose.connection.once('open', function() {
